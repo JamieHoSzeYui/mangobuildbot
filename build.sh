4f6a200286ddc9ad424034d8d3fa60fbce053e8c
JamieHoSzeYui <wileylau@gmail.com>
2020-09-08 20:27:23 +0800

    Fixup build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd /mango414
 echo "cloning toolchain..."
 git clone https://github.com/mscalindt/aarch64-linux-android-4.9.git --depth=1
 CROSS_COMPILE+="aarch64-linux-android-4.9/bin/aarch64-linux-android-"
